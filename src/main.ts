5a848403c748466c88db2c938de13e5252dcd4e0
joysarkar913 <76178347+joysarkar913@users.noreply.github.com>
2026-09-06 23:46:13 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
-  await app.listen(3020);
+  await app.listen(process.env.PORT ??3000);
 }
 await bootstrap();
