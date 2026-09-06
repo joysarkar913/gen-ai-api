536cdde10a5f4f4fe3469032f33c7ffa49b06fa0
joysarkar913 <76178347+joysarkar913@users.noreply.github.com>
2026-09-06 23:47:09 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    // Replace with the exact frontend URL allowed to access the API
-    origin: 'engineer-joy.web.app', // e.g. React, Next.js, or Vite dev server
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   // Replace with the exact frontend URL allowed to access the API
+  //   origin: 'engineer-joy.web.app', // e.g. React, Next.js, or Vite dev server
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   credentials: true,
+  // });
   await app.listen(process.env.PORT ??3000);
 }
 await bootstrap();
