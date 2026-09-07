2900fd8a7aa6db32845fe50fc3bdc28dbb0f47a6
joysarkar913 <76178347+joysarkar913@users.noreply.github.com>
2026-09-07 07:59:13 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { ExpressAdapter } from '@nestjs/platform-express'; 
+import express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const expressApp = express();
+  const app = await NestFactory.create(AppModule,new ExpressAdapter(expressApp));
   app.enableCors({
     // Replace with the exact frontend URL allowed to access the API
     origin: 'engineer-joy.web.app', // e.g. React, Next.js, or Vite dev server
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
-  await app.listen(process.env.PORT ??3000);
+  await app.init(); 
+  return expressApp;
 }
-await bootstrap();
+const handler = bootstrap();
+export default handler;
+
